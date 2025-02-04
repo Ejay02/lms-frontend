419270c457e711565113feec2537daba60069e97
Ejay02 <e.jae02@gmail.com>
2025-02-04 09:17:40 +0100

    feat: update Navbar import to match file naming convention

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from "vue-router";
-import Navbar from "./components/Navbar.vue";
+import Navbar from "./components/navbar.vue";
 import NotificationsContainer from "./components/ui/notificationContainer.vue";
 </script>
 
